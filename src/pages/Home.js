05e3923c1f637d9ac089383cf12f80b8aeed1eb4
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-06-14 23:00:48 -0300

    implemented Submit onChange

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './Home.css'
 
 const HomePage = () => {
+
+
   return (
     <div className='container__home'>
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 
+function initialState() {
+  return { user: '', password: '' }
+}
+
 const LoginPage = () => {
+  const [values, setValues] = useState(initialState)
+
+  function onChange(event) {
+    const { value, name } = event.target
+
+    setValues({
+      ...values,
+      [name]: value
+    })
+  }
+
   return (
     <div className='container__login'>
-
       <form className='form__login'>
         <h1>ioasys books</h1>
         <div>
-          <label>Email</label>
-          <input type='email' name='email' />
+          <label htmlFor='user'>Email</label>
+          <input id='user' type='text' name='user' onChange={onChange} value={values.user} />
         </div>
         <div>
-          <label>Senha</label>
-          <input type='password' name='password' />
+          <label htmlFor='password'>Senha</label>
+          <input id='password' type='password' name='password' onChange={onChange} value={values.password} />
           <button className='btn__login' type='submit'>Entrar</button>
         </div>
       </form>
